edb9548f46d45727d3590442ff74db54a708af20
Benjamin Bach <benjamin@overtag.dk>
2021-01-10 22:37:04 +0100

    Fix basic test

--- test/test_basic.py
+++ test/test_basic.py
@@ -1,3 +1,7 @@
+import pytest
+
+
+@pytest.mark.django_db(transaction=True)
 def test_with_client(client):
-    response = client.get('/en/')
+    response = client.get("/en/")
     assert response.status_code == 200
