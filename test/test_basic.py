ffc7c81f565fdddd4062f62d5a7d0eb507876878
Benjamin Bach <benjamin@overtag.dk>
2021-04-25 21:20:40 +0200

    Add a test of about page

--- test/test_basic.py
+++ test/test_basic.py
@@ -3,8 +3,14 @@ from django.core.management import call_command
 
 
 @pytest.mark.django_db(transaction=True)
-def test_with_client(client):
+def test_landing_page(client):
     response = client.get("/en/")
+    assert response.status_code == 200
+
+
+@pytest.mark.django_db(transaction=True)
+def test_about_page(client):
+    response = client.get("/en/news/about/")
     assert response.status_code == 200
 
 
