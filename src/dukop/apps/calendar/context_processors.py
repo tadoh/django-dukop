af3f87cd6c487abd1f8365d99cdb69f5e9d987f4
Benjamin Bach <benjamin@overtag.dk>
2021-05-25 01:26:34 +0200

    remove print stm

--- src/dukop/apps/calendar/context_processors.py
+++ src/dukop/apps/calendar/context_processors.py
@@ -2,7 +2,6 @@ from . import models
 
 
 def dukop_sphere(request):
-    print(models.Sphere.get_all_cached)
     return {
         "SPHERE": request.sphere,
         "SPHERES": models.Sphere.get_all_cached(),
