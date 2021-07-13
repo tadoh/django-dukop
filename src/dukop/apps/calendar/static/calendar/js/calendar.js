06e1132ef52c563222905371a19c1bb1069e4786
Benjamin Bach <benjamin@overtag.dk>
2021-07-13 22:55:55 +0200

    Remove modal windows from calendar index

--- src/dukop/apps/calendar/static/calendar/js/calendar.js
+++ src/dukop/apps/calendar/static/calendar/js/calendar.js
@@ -1,3 +1,7 @@
+/*
+Zero-framework JS stuff
+*/
+
 function closeAll() {
   document.getElementById('js-overlay').classList.remove('js-active');
   document.getElementById('js-lock-body').classList.remove('js-active');
@@ -9,6 +13,14 @@ function closeAll() {
 }
 
 function ready(event) {
+
+  paginator_select = document.getElementById('paginator');
+  if (paginator_select) {
+    paginator_select.addEventListener('change', (event) => {
+      location.href = document.querySelector('.result');
+    });
+  }
+
   function addEventListenerList(list, event, fn) {
     for (var i = 0, len = list.length; i < len; i++) {
       list[i].addEventListener(event, fn, false);
@@ -22,50 +34,23 @@ function ready(event) {
     element.classList.toggle('js-active');
   }
 
-  document.getElementById('paginator').addEventListener('change', (event) => {
-    location.href = document.querySelector('.result');
-  });
-
-  window.onhashchange = function () {
-    console.log("window hash changed to", window.location.hash);
-  };
-
-  function toggleCard(event) {
-    event.preventDefault();
-    this.parentNode.classList.toggle('js-active');
-    document.getElementById('js-overlay').classList.toggle('js-active');
-    document.getElementById('js-lock-body').classList.toggle('js-active');
-
-    // Change active URL
-    if (this.parentNode.classList.contains('js-active')) {
-       // prevents browser from storing history with each change:
-       window.location.hash = this.parentNode.dataset.hash;
-       window.scrollTo(0,0);
-
-    }
-  }
-
-  function toggleBurger() {
-    var controls = document.getElementById('js-controls');
-    controls.classList.toggle('js-active');
-  }
-
   var listJsToggle = document.getElementsByClassName('js-toggle');
   addEventListenerList(listJsToggle, 'click', toggleActive);
 
-  var listJsToggleCard = document.getElementsByClassName('js-toggle-card');
-  addEventListenerList(listJsToggleCard, 'click', toggleCard);
 
   var now = new Date();
   var nowElement = document.getElementById('timeline__now');
   var timeAsNumber = now.getHours() + (now.getMinutes() / 60);
   nowElement.style.marginLeft = (timeAsNumber - 8) * 6.25 + '%';
+
   // clean up event binding
   window.removeEventListener('DOMContentLoaded', ready);
 }
+
 // bind to the load event
 window.addEventListener('DOMContentLoaded', ready);
 
+/*
 document.onkeydown = function (evt) {
   evt = evt || window.event;
   var isEscape = false;
@@ -78,3 +63,4 @@ document.onkeydown = function (evt) {
     closeAll();
   }
 };
+*/
