df42463f9f616c4fd800bc823180719796a2880f
lars <lg@goforlag.dk>
2021-10-12 22:25:00 +0200

    update event_card
    Layout changes
    and clipboard

--- src/dukop/static/js/main.js
+++ src/dukop/static/js/main.js
@@ -2,3 +2,13 @@ $("input.share-text-input").focus(function() {
    $(this).select();
    $(this).attr("readonly", true);
 });
+
+function copy() {
+   var copyText = document.querySelector("input.share-text-input");
+   copyText.select();
+   document.execCommand("copy");
+ }
+ 
+if (document.querySelector(".js-copy")) {
+   document.querySelector(".js-copy").addEventListener("click", copy);
+}
