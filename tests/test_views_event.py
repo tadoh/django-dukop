479a1eac5783c0521360c2ceb1a2ed0c1a605bf3
Benjamin Bach <benjamin@overtag.dk>
2021-11-01 22:09:36 +0100

    Add integration test of recurring event

--- tests/test_views_event.py
+++ tests/test_views_event.py
@@ -5,19 +5,16 @@ import pytest
 from django.urls.base import reverse
 from django.utils import timezone
 from dukop.apps.calendar import forms
-from dukop.apps.calendar.models import Event
-from dukop.apps.calendar.models import Sphere
+from dukop.apps.calendar import models
 
 from .fixtures_users import single_user  # noqa
 from .fixtures_users import SINGLE_USER_PASSWORD
 
 
-@pytest.mark.django_db(transaction=True)
-def test_create_event(client, single_user):  # noqa
-    client.login(username=single_user.email, password=SINGLE_USER_PASSWORD)
-    response = client.get(reverse("calendar:event_create"))
-    assert response.status_code == 200
-
+def create_form_data(response):
+    """
+    A utility function that generates the data for an integration test of EventForm
+    """
     # data will receive all the forms field names
     # key will be the field name (as "formx-fieldname"), value will be the string representation.
     data = {}
@@ -41,6 +38,21 @@ def test_create_event(client, single_user):  # noqa
                 data["%s-%s" % (current_form.prefix, field_name)] = (
                     value if value is not None else ""
                 )
+    data["spheres"] = models.Sphere.get_default().pk
+
+    return data
+
+
+@pytest.mark.django_db(transaction=True)
+def test_create_event(client, single_user):  # noqa
+    """
+    Test basic event creation
+    """
+    client.login(username=single_user.email, password=SINGLE_USER_PASSWORD)
+    response = client.get(reverse("calendar:event_create"))
+    assert response.status_code == 200
+
+    data = create_form_data(response)
 
     first_times_form = response.context["times"].forms[0]
     start_datetime = timezone.now() + timedelta(days=1)
@@ -52,15 +64,62 @@ def test_create_event(client, single_user):  # noqa
     event_name = f"Test event {random.randint(10000, 99999)}"
 
     data["name"] = event_name
-    data["spheres"] = Sphere.objects.all()[0].pk
     data["host_choice"] = forms.CreateEventForm.HOST_NEW
     data["location_choice"] = forms.EventForm.LOCATION_NEW
 
     data["new_host"] = "Test group {random.randint(10000, 99999)}"
 
-    print(data)
-
     response = client.post(reverse("calendar:event_create"), data=data)
     assert response.status_code == 302
 
-    assert Event.objects.all().latest("id").name == event_name
+    assert models.Event.objects.all().latest("id").name == event_name
+
+
+@pytest.mark.django_db(transaction=True)
+def test_create_recurring_event(client, single_user):  # noqa
+    """
+    Creates an event that recurs weekly
+    """
+    client.login(username=single_user.email, password=SINGLE_USER_PASSWORD)
+    response = client.get(reverse("calendar:event_create"))
+    assert response.status_code == 200
+
+    data = create_form_data(response)
+
+    first_times_form = response.context["times"].forms[0]
+    start_datetime = timezone.now() + timedelta(days=1)
+    data["%s-%s_0" % (first_times_form.prefix, "start")] = start_datetime.date()
+    data["%s-%s_1" % (first_times_form.prefix, "start")] = start_datetime.strftime(
+        "%H:%M"
+    )
+
+    first_recurrences_form = response.context["recurrences"].forms[0]
+    end_datetime = timezone.now() + timedelta(days=100)
+
+    data["%s-%s_0" % (first_recurrences_form.prefix, "end")] = end_datetime.date()
+    data["%s-%s_1" % (first_recurrences_form.prefix, "end")] = end_datetime.strftime(
+        "%H:%M"
+    )
+    data[
+        "%s-%s" % (first_recurrences_form.prefix, "interval_type")
+    ] = models.EventRecurrence.RECURRENCE_TYPES[0][0]
+
+    event_name = f"Test event {random.randint(10000, 99999)}"
+
+    data["name"] = event_name
+    data["host_choice"] = forms.CreateEventForm.HOST_NEW
+    data["location_choice"] = forms.EventForm.LOCATION_NEW
+
+    data["new_host"] = "Test group {random.randint(10000, 99999)}"
+
+    response = client.post(reverse("calendar:event_create"), data=data)
+
+    try:
+        assert response.status_code == 302
+    except AssertionError:
+        print(response.context["form"].errors)
+        print(response.context["recurrences"].errors)
+        raise
+
+    assert models.Event.objects.all().latest("id").name == event_name
+    assert models.Event.objects.all().latest("id").times.all().count() >= 100 // 7
